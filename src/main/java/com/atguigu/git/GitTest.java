99ac7d6818a5638110480afeaad8edf551d10057
weiguanghao <1525658286@qq.com>
2023-02-23 10:41:31 +0800

    github test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,8 @@ public class GitTest {
         System.out.println("pull test");
         System.out.println("gitee test");
         System.out.println("gitee test1111");
+        System.out.println("hub test1111");
+        System.out.println("github test1111");
+        System.out.println("github test1111");
     }
 }
