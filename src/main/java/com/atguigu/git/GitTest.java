14ab7b63979fb8f8d16eee7644409f5dec5851c6
weiguanghao <1525658286@qq.com>
2023-02-23 08:35:53 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Guanghao Wei
+ * @create 2023-02-23 8:33
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
